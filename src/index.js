e0dc3ed808a992cc7c1be4ad7cef44ea8ed226cb
John Gualteros <gualterosjohn40@gmail.com>
2022-07-14 20:07:07 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+// import ejs from 'ejs'; ExpressJS Template Engine
+import {dirname,join} from 'path';
+import { fileURLToPath } from 'url';
+import indexRoutes from "./routes/index.js";
+const app=express();
+
+const __dirname=dirname(fileURLToPath(import.meta.url));
+
+app.set('views',join(__dirname,'views'));
+app.set('view engine','pug');
+app.use(indexRoutes);
+
+app.use(express.static(join(__dirname,'public')));
+
+
+app.listen(3000)
---
+++ src/routes/index.js
@@ -0,0 +1,4 @@
+import { Router } from "express";
+const router = Router();
+router.get("/", (req, res) => res.render("index"));
+export default router;
